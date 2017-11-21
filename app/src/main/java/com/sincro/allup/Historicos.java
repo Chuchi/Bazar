9726ff2e930e7c6214a1739f64d3c9f9c6b0c85b
Chuchi <fabricagd>
2017-11-21 02:36:46 -0400

    Con luopa

--- app/src/main/java/com/sincro/allup/Historicos.java
+++ app/src/main/java/com/sincro/allup/Historicos.java
@@ -1,19 +1,29 @@
 package com.sincro.allup;
 
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class Historicos extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Historicos extends AppCompatActivity implements View.OnClickListener {
 
 
     TextView TXV40, TXV41, TXV42, TXV43, TXV44;
-    Button BTN40,BTN41;
+    Button BTN41;
     Spinner SPNN40, SPNN41,SPNN43, SPNN42;
+    ImageButton IMBTN114, IMBTN252 ;
     ListView LV40;
     ArrayAdapter<String> Lines, States, Ares ;
     @Override
@@ -34,9 +44,13 @@ public class Historicos extends AppCompatActivity {
         SPNN42=(Spinner) findViewById(R.id.SPNN42) ;
      // SPNN43=(Spinner) findViewById(R.id.SPNN43) ;
 
+        IMBTN114=(ImageButton)findViewById(R.id.IMBTN114) ;
+        IMBTN252=(ImageButton)findViewById(R.id.IMBTN252) ;
 
-        BTN40 = (Button) findViewById(R.id.BTN40);
         BTN41 = (Button) findViewById(R.id.BTN41);
+
+        IMBTN114.setOnClickListener(this);
+        BTN41.setOnClickListener(this);
 
 
         Lines = new ArrayAdapter<String>(this,R.layout.stilotexto2,getResources().getStringArray(R.array.Lineaxxx));
@@ -47,5 +61,43 @@ public class Historicos extends AppCompatActivity {
         SPNN40.setAdapter(Lines);
         SPNN41.setAdapter(Ares);
         SPNN42.setAdapter(States);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.BTN41:
+                    finish();
+                break;
+
+        }
+    }
+
+    public void MostrarEscogedorFecha(View v) {
+        DialogFragment newFragment = new DialogoEscogerFecha();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DialogoEscogerFecha extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+
     }
 }
