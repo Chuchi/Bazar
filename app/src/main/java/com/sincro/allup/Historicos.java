209bd258766aa1890d0dacd588f605050fc64d90
Chuchi <fabricagd>
2017-11-22 13:52:15 -0400

    Despues de almorzar

--- app/src/main/java/com/sincro/allup/Historicos.java
+++ app/src/main/java/com/sincro/allup/Historicos.java
@@ -3,6 +3,7 @@ package com.sincro.allup;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -20,12 +21,14 @@ import java.util.Calendar;
 public class Historicos extends AppCompatActivity implements View.OnClickListener {
 
 
-    TextView TXV40, TXV41, TXV42, TXV43, TXV44;
+    TextView TXV40, TXV41, TXV42, TXV51, TXV52;
     Button BTN41;
-    Spinner SPNN40, SPNN41,SPNN43, SPNN42;
-    ImageButton IMBTN114, IMBTN252 ;
+    Spinner SPNN40, SPNN41, SPNN43, SPNN42;
+    ImageButton IMBTN114, IMBTN252, IMBTN253;
     ListView LV40;
-    ArrayAdapter<String> Lines, States, Ares ;
+    ArrayAdapter<String> Lines, States, Ares;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,27 +38,30 @@ public class Historicos extends AppCompatActivity implements View.OnClickListene
         TXV40 = (TextView) findViewById(R.id.TXV40);
         TXV41 = (TextView) findViewById(R.id.TXV41);
         TXV42 = (TextView) findViewById(R.id.TXV42);
-       // TXV43 = (TextView) findViewById(R.id.TXV43);
-     //   TXV44 = (TextView) findViewById(R.id.TXV44);
+        TXV51 = (TextView) findViewById(R.id.TXV51);
+        TXV52 = (TextView) findViewById(R.id.TXV52);
 
 
-        SPNN40=(Spinner) findViewById(R.id.SPNN40) ;
-        SPNN41=(Spinner) findViewById(R.id.SPNN41) ;
-        SPNN42=(Spinner) findViewById(R.id.SPNN42) ;
-     // SPNN43=(Spinner) findViewById(R.id.SPNN43) ;
+        SPNN40 = (Spinner) findViewById(R.id.SPNN40);
+        SPNN41 = (Spinner) findViewById(R.id.SPNN41);
+        SPNN42 = (Spinner) findViewById(R.id.SPNN42);
+        // SPNN43=(Spinner) findViewById(R.id.SPNN43) ;
 
-        IMBTN114=(ImageButton)findViewById(R.id.IMBTN114) ;
-        IMBTN252=(ImageButton)findViewById(R.id.IMBTN252) ;
+        IMBTN114 = (ImageButton) findViewById(R.id.IMBTN114);
+        IMBTN252 = (ImageButton) findViewById(R.id.IMBTN252);
+        IMBTN253 = (ImageButton) findViewById(R.id.IMBTN253);
 
         BTN41 = (Button) findViewById(R.id.BTN41);
 
         IMBTN114.setOnClickListener(this);
+        IMBTN252.setOnClickListener(this);
+        IMBTN253.setOnClickListener(this);
         BTN41.setOnClickListener(this);
 
 
-        Lines = new ArrayAdapter<String>(this,R.layout.stilotexto2,getResources().getStringArray(R.array.Lineaxxx));
-        States = new ArrayAdapter<String>(this,R.layout.stilotexto2,getResources().getStringArray(R.array.Estadox));
-        Ares = new ArrayAdapter<String>(this,R.layout.stilotexto2,getResources().getStringArray(R.array.Areax));
+        Lines = new ArrayAdapter<String>(this, R.layout.stilotexto2, getResources().getStringArray(R.array.Lineaxxx));
+        States = new ArrayAdapter<String>(this, R.layout.stilotexto2, getResources().getStringArray(R.array.Estadox));
+        Ares = new ArrayAdapter<String>(this, R.layout.stilotexto2, getResources().getStringArray(R.array.Areax));
 
 
         SPNN40.setAdapter(Lines);
@@ -69,19 +75,32 @@ public class Historicos extends AppCompatActivity implements View.OnClickListene
         switch (view.getId()) {
 
             case R.id.BTN41:
-                    finish();
+                finish();
+                break;
+            case R.id.IMBTN252:
+                MostrarEscogedorFecha();
+                break;
+            case R.id.IMBTN253:
+                MostrarEscogedorFecha();
+                break;
+            case R.id.IMBTN114:
+
                 break;
 
         }
     }
 
-    public void MostrarEscogedorFecha(View v) {
+
+    public void MostrarEscogedorFecha() {
         DialogFragment newFragment = new DialogoEscogerFecha();
+        //       newFragment.init(this, this);
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
-    public static class DialogoEscogerFecha extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
+
+
+    public static class DialogoEscogerFecha extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+        Context pedro;
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -95,9 +114,20 @@ public class Historicos extends AppCompatActivity implements View.OnClickListene
             return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
+
+
+        @Override
         public void onDateSet(DatePicker view, int year, int month, int day) {
             // Do something with the date chosen by the user
-        }
 
+
+    //   ((Activity)view.getContext())(TXV51).setAlpha(0.2f);//
+     //   tormento.setText("tormenta");
+        }
     }
 }
+
+
+
+
+
